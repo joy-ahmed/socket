661ea2d277e461d2e6496584afaa6452a2506238
Joy Ahmed <joy.ahmed.dev@gmail.com>
2026-01-15 20:31:08 +0600

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected! 🔌");
+});
+
+server.listen(3000, () => {
+  console.log("Server running");
+});
