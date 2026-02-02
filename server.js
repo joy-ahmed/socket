9da8e757429efe589591d9dab09e07d552947b08
Joy Ahmed <joy.ahmed.dev@gmail.com>
2026-02-02 22:50:12 +0600

    wss configured

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+//communication Event
+wss.on("connection", (socket, request) => {
+  const ip = request.socket.remoteAddress;
+  socket.on("message", (rawData) => {
+    const message = rawData.toString();
+    console.log({ rawData });
+    wss.clients.forEach((client) => {
+      if (client.readyState === 1) client.send(`Server Broadcast: ${message}`);
+    });
+  });
+  //catching error
+  socket.on("error", (err) => {
+    console.log(`Error: ${err.message}: ${ip}`);
+  });
+  //check if offline
+  socket.on("close", () => {
+    console.log("client disconnected.");
+  });
+});
